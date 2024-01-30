2ac747c25913f4e2172b8f5547b586c9dca11eb6
Shepherd <shepherd0619@outlook.com>
2024-01-30 18:29:39 +0800

    [fix] ReturnCleanASCII may delete non-English character

--- WebSocketUtility.cs
+++ WebSocketUtility.cs
@@ -162,8 +162,8 @@ public class WebSocketUtility
 		StringBuilder sb = new StringBuilder(s.Length);
 		foreach (char c in s)
 		{
-			if ((int)c > 127) // you probably don't want 127 either
-				continue;
+			//if ((int)c > 127) // you probably don't want 127 either
+			//	continue;
 			if ((int)c < 32)  // I bet you don't want control characters 
 				continue;
 			if (c == '%')
